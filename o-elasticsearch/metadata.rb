8ef818c4982579b8bf7ff1f566c0205181de42e5
Stuart Warren <stuart.warren@ocado.com>
2014-04-25 18:07:07 +0100

    test fix to missing recipe

--- o-elasticsearch/metadata.rb
+++ o-elasticsearch/metadata.rb
@@ -4,4 +4,6 @@ maintainer  "stuart-warren"
 license     "Apache 2.0"
 version     "0.0.1"
 
-depends 'elasticsearch'
+depends "elasticsearch"
+recipe "o-elasticsearch::datanode", "Sets up a data only node"
+recipe "o-elasticsearch::httpnode", "Sets up a http only node"
--- o-logstash/metadata.rb
+++ o-logstash/metadata.rb
@@ -4,4 +4,4 @@ maintainer  "stuart-warren"
 license     "Apache 2.0"
 version     "0.0.1"
 
-depends 'logstash'
+depends "logstash"
