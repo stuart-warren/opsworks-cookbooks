98f6c6f5f8e4164ae9565395043d18b655b83319
Stuart Warren <stuart.warren@ocado.com>
2014-04-28 17:56:43 +0100

    tweak recipies to include more elasticsearch ones

--- o-elasticsearch/attributes/datanode.rb
+++ o-elasticsearch/attributes/datanode.rb
@@ -1,6 +1,6 @@
 
-set['elasticsearch']['version'] = '1.1.0'
-set['elasticsearch']['node']['master'] = false
-set['elasticsearch']['node']['data'] = true
-set['elasticsearch']['custom_config']['indices.memory.index_buffer_size'] = '60%'
-set['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
+# set['elasticsearch']['version'] = '1.1.0'
+# set['elasticsearch']['node']['master'] = false
+# set['elasticsearch']['node']['data'] = true
+# set['elasticsearch']['custom_config']['indices.memory.index_buffer_size'] = '60%'
+# set['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
--- o-elasticsearch/recipes/datanode.rb
+++ o-elasticsearch/recipes/datanode.rb
@@ -1,2 +1,8 @@
 
 include_recipe 'o-elasticsearch::default'
+
+node.override['elasticsearch']['version'] = '1.1.0'
+node.override['elasticsearch']['node']['master'] = false
+node.override['elasticsearch']['node']['data'] = true
+node.override['elasticsearch']['custom_config']['indices.memory.index_buffer_size'] = '60%'
+node.override['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
