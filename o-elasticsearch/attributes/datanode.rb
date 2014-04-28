89d50871eb4d48cff49acf87ef361d20b21dad66
Stuart Warren <stuart.warren@ocado.com>
2014-04-28 16:23:36 +0100

    tweak recipies to include more elasticsearch ones

--- o-elasticsearch/attributes/datanode.rb
+++ o-elasticsearch/attributes/datanode.rb
@@ -1,4 +1,4 @@
 
-normal['elasticsearch']['node']['master'] = false
-normal['elasticsearch']['node']['data'] = true
-normal['elasticsearch']['custom_config']['indices.memory.index_buffer_size'] = '60%'
+set['elasticsearch']['node']['master'] = false
+set['elasticsearch']['node']['data'] = true
+set['elasticsearch']['custom_config']['indices.memory.index_buffer_size'] = '60%'
--- o-elasticsearch/attributes/httpnode.rb
+++ o-elasticsearch/attributes/httpnode.rb
@@ -1,9 +1,9 @@
 
-normal['elasticsearch']['node']['master'] = true
-normal['elasticsearch']['node']['data'] = false
-normal['elasticsearch']['nginx']['allow_cluster_api'] = true
-normal['elasticsearch']['nginx']['port'] = 80
-normal['elasticsearch']['plugins']['karmi/elasticsearch-paramedic'] = {}
-normal['elasticsearch']['plugins']['lmenezes/elasticsearch-kopf']['version'] = '0.5.5'
-normal['elasticsearch']['plugins']['royrusso/elasticsearch-HQ'] = {}
-normal['elasticsearch']['plugins']['karmi/elasticsearch-paramedic'] = {}
+set['elasticsearch']['node']['master'] = true
+set['elasticsearch']['node']['data'] = false
+set['elasticsearch']['nginx']['allow_cluster_api'] = true
+set['elasticsearch']['nginx']['port'] = 80
+set['elasticsearch']['plugins']['karmi/elasticsearch-paramedic'] = {}
+set['elasticsearch']['plugins']['lmenezes/elasticsearch-kopf']['version'] = '0.5.5'
+set['elasticsearch']['plugins']['royrusso/elasticsearch-HQ'] = {}
+set['elasticsearch']['plugins']['karmi/elasticsearch-paramedic'] = {}
