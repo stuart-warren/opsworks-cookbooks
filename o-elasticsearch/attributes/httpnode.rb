d71185f05ce636ef53c6ca7c97dd169e1c4d217d
Stuart Warren <stuart.warren@ocado.com>
2014-04-28 16:50:32 +0100

    tweak recipies to include more elasticsearch ones

--- o-elasticsearch/attributes/httpnode.rb
+++ o-elasticsearch/attributes/httpnode.rb
@@ -1,8 +1,10 @@
 
+set['elasticsearch']['version'] = '1.1.0'
 set['elasticsearch']['node']['master'] = true
 set['elasticsearch']['node']['data'] = false
 set['elasticsearch']['nginx']['allow_cluster_api'] = true
 set['elasticsearch']['nginx']['port'] = 80
+set['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
 set['elasticsearch']['plugins']['karmi/elasticsearch-paramedic'] = {}
 set['elasticsearch']['plugins']['lmenezes/elasticsearch-kopf']['version'] = '0.5.5'
 set['elasticsearch']['plugins']['royrusso/elasticsearch-HQ'] = {}
