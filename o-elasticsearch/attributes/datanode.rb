b266a675a2bb4b12c2c631feff620db7c3ba1dec
Stuart Warren <stuart.warren@ocado.com>
2014-04-29 12:52:06 +0100

    test fix to wrapper cookbook

--- o-elasticsearch/attributes/datanode.rb
+++ o-elasticsearch/attributes/datanode.rb
@@ -1,6 +1,8 @@
 
-default!['elasticsearch']['version'] = '1.1.0'
-default!['elasticsearch']['node']['master'] = false
-default!['elasticsearch']['node']['data'] = true
-default!['elasticsearch']['custom_config']['indices.memory.index_buffer_size'] = '60%'
-default!['elasticsearch']['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
+include_attribute 'o-elasticsearch::default'
+
+default.elasticsearch['version'] = '1.1.0'
+default.elasticsearch['node']['master'] = false
+default.elasticsearch['node']['data'] = true
+default.elasticsearch['custom_config']['indices.memory.index_buffer_size'] = '60%'
+default.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
