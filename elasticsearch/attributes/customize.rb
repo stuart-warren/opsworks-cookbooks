d60c71408dab34e01455aae662a778288a6c17c6
Stuart Warren <stuart.warren@ocado.com>
2014-04-29 16:36:58 +0100

    tweak attributes

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,26 +1,30 @@
 
 
 normal.elasticsearch['version'] = '1.1.0'
-normal.elasticsearch['cluster']['name'] = 'elasticsearch'
-normal.elasticsearch['node']['master'] = true
-normal.elasticsearch['node']['data'] = true
-normal.elasticsearch['logging']['action.discovery.ec2'] = 'WARN'
+normal.elasticsearch['cluster']['name'] = 'elasticsearch-aws'
+# normal.elasticsearch['node']['master'] = true
+# normal.elasticsearch['node']['data'] = true
+normal.elasticsearch['logging']['action.discovery.ec2'] = 'INFO'
 normal.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
 normal.elasticsearch['cloud']['aws']['region'] = 'eu-west-1'
 normal.elasticsearch['index']['number_of_shards'] = 3
 normal.elasticsearch['index']['number_of_replicas'] = 2
 normal.elasticsearch['discovery']['type'] = 'ec2'
-normal.elasticsearch['discovery']['zen']['minimum_master_nodes'] = 1
-normal.elasticsearch['discovery']['ping']['multicast']['enabled'] = false
-normal.elasticsearch['discovery']['ec2']['tag']['opsworks:stack'] = "elasticsearch"
+# Should probably be cleverer and work out the number of masters?
+normal.elasticsearch['discovery']['zen']['minimum_master_nodes'] = 2
+normal.elasticsearch['discovery']['zen']['ping']['multicast']['enabled'] = false
+normal.elasticsearch['discovery']['ec2']['tag']['opsworks:stack'] = "elasticsearch-test"
 normal.elasticsearch['limits']['nofile'] = 655350
 normal.elasticsearch['custom_config']['cluster.routing.allocation.awareness.attributes'] = 'aws_availability_zone'
 normal.elasticsearch['custom_config']['index.refresh_interval'] = '3s'
 normal.elasticsearch['custom_config']['indices.fielddata.cache.size'] = '15%'
 normal.elasticsearch['custom_config']['indices.fielddata.cache.expire'] = '1h'
 normal.elasticsearch['custom_config']['indices.cache.filter.size'] = '15%'
+normal.elasticsearch['custom_config']['action.disable_shutdown'] = true
+normal.elasticsearch['custom_config']['script.disable_dynamic'] = true
 
 
+# For nodes within this layer
 if node[:opsworks][:instance][:layers].include? 'elasticsearch-frontend'
 
     # httpnode
@@ -35,11 +39,13 @@ if node[:opsworks][:instance][:layers].include? 'elasticsearch-frontend'
 
 end
 
+# For nodes within this other layer
 if node[:opsworks][:instance][:layers].include? 'elasticsearch-data'
 
     # datanode
     normal.elasticsearch['node']['master'] = false
     normal.elasticsearch['node']['data'] = true
     normal.elasticsearch['custom_config']['indices.memory.index_buffer_size'] = '60%'
+    normal.elasticsearch['custom_config']['http.enabled'] = false
 
 end
