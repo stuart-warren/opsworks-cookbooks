b2f35dbbe06aecec95b45d419d161e0f2385295c
Stuart Warren <stuart.warren@ocado.com>
2014-04-29 17:16:31 +0100

    test logstash customized_attributes

---
+++ logstash/attributes/customize.rb
@@ -0,0 +1,12 @@
+
+normal.logstash['server']['enable_embedded_es'] = false
+# normal.logstash['server']['filters'] = [{}]
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['host'] = 'elasticsearch-frontend-946485335.eu-west-1.elb.amazonaws.com'
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['port'] = '9200'
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['flush_size'] = 500
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['idle_flush_time'] = 1
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['replication'] = 'sync'
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['workers'] = 3
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['index'] = "aws-logstash-%{+YYYY.MM.dd}"
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['manage_templates'] = false
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['codec'] = 'json'
