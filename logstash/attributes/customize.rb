e5e1a4c58392d394e39134048b9d16e40eaea91a
Stuart Warren <stuart.warren@ocado.com>
2014-04-30 11:15:58 +0100

    test logstash customized_attributes

--- logstash/attributes/customize.rb
+++ logstash/attributes/customize.rb
@@ -1,18 +1,18 @@
 
-normal.logstash['instance']['server']['enable_embedded_es'] = false
+normal.logstash['server']['enable_embedded_es'] = false
 
-normal.logstash['instance']['server']['elasticsearch_ip'] = '127.0.0.1'
-normal.logstash['instance']['server']['elasticsearch_query'] = 'stack:elasticsearch*'
-normal.logstash['instance']['server']['graphite_ip'] = '127.0.0.1'
-normal.logstash['instance']['server']['graphite_query'] = 'stack:graphite*'
+normal.logstash['server']['elasticsearch_ip'] = '127.0.0.1'
+normal.logstash['server']['elasticsearch_query'] = 'stack:elasticsearch*'
+normal.logstash['server']['graphite_ip'] = '127.0.0.1'
+normal.logstash['server']['graphite_query'] = 'stack:graphite*'
 
-# normal.logstash['instance']['server']['filters'] = [{}]
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['host'] = 'elasticsearch-frontend-946485335.eu-west-1.elb.amazonaws.com'
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['port'] = '9200'
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['flush_size'] = 500
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['idle_flush_time'] = 1
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['replication'] = 'sync'
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['workers'] = 3
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['index'] = "aws-logstash-%{+YYYY.MM.dd}"
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['manage_templates'] = false
-normal.logstash['instance']['server']['outputs'][0]['elasticsearch_http']['codec'] = 'json'
+# normal.logstash['server']['filters'] = [{}]
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['host'] = 'elasticsearch-frontend-946485335.eu-west-1.elb.amazonaws.com'
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['port'] = '9200'
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['flush_size'] = 500
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['idle_flush_time'] = 1
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['replication'] = 'sync'
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['workers'] = 3
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['index'] = "aws-logstash-%{+YYYY.MM.dd}"
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['manage_templates'] = false
+normal.logstash['server']['outputs'][0]['elasticsearch_http']['codec'] = 'json'
