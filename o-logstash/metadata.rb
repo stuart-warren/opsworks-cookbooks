7d545746a47342ec7f80bc9296f78f148b5daffd
Stuart Warren <stuart.warren@ocado.com>
2014-04-30 19:33:05 +0100

    test logstash customized_attributes

--- o-logstash/metadata.rb
+++
@@ -1,7 +0,0 @@
-name        "o-logstash"
-description "Wrapper for logstash cookbook to provide attributes"
-maintainer  "stuart-warren"
-license     "Apache 2.0"
-version     "0.0.1"
-
-depends "logstash"
