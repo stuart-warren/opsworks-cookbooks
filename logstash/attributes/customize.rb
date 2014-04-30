a5a0d431f3ca4c48de8e00e5e2286c00a1d4ef18
Stuart Warren <stuart.warren@ocado.com>
2014-04-30 19:32:48 +0100

    test logstash customized_attributes

--- logstash/attributes/customize.rb
+++ logstash/attributes/customize.rb
@@ -4,7 +4,7 @@ normal['platform_version'] = '6.0'
 normal.logstash['install_zeromq'] = true
 normal.logstash['server']['enable_embedded_es'] = false
 
-# normal.logstash['server']['elasticsearch_ip'] = '127.0.0.1'
+# normal.logstash['server']['elasticsearch_ip'] = 'elasticsearch-frontend-000000.eu-west-1.elb.amazonaws.com'
 # normal.logstash['server']['elasticsearch_query'] = 'stack:elasticsearch*'
 # normal.logstash['server']['graphite_ip'] = '127.0.0.1'
 # normal.logstash['server']['graphite_query'] = 'stack:graphite*'
@@ -13,19 +13,8 @@ normal.logstash['server']['config_templates'] = ['o_server']
 
 normal.logstash['server']['config_templates_variables'] = { o_server: { zmq_port: 2120,
                                                                         zmq_sockopts: '"ZMQ::RCVHWM",1000,"ZMQ::LINGER",10000, "ZMQ::RCVBUF", 87380',
-                                                                        es_server_ip: 'elasticsearch-frontend-946485335.eu-west-1.elb.amazonaws.com',
-                                                                        es_port: 443,
+                                                                        es_port: 80,
                                                                         es_index_prd: "aws-logstash-%{+YYYY.MM.dd}",
                                                                         es_flush_size: 500,
                                                                         es_idle_flush: 1,
                                                                         es_codec: 'json' } }
-# normal.logstash['server']['filters'] = [{}]
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['host'] = 'elasticsearch-frontend-946485335.eu-west-1.elb.amazonaws.com'
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['port'] = '9200'
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['flush_size'] = 500
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['idle_flush_time'] = 1
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['replication'] = 'sync'
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['workers'] = 3
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['index'] = "aws-logstash-%{+YYYY.MM.dd}"
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['manage_templates'] = false
-# normal.logstash['server']['outputs'][0]['elasticsearch_http']['codec'] = 'json'
