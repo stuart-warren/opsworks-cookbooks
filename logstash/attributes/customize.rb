60d3ad3028588ae903849d4e80c1203a8371c131
Stuart Warren <stuart.warren@ocado.com>
2014-04-30 14:41:38 +0100

    test logstash customized_attributes

--- logstash/attributes/customize.rb
+++ logstash/attributes/customize.rb
@@ -1,14 +1,15 @@
 
+normal.logstash['install_zeromq'] = true
 normal.logstash['server']['enable_embedded_es'] = false
 
-normal.logstash['server']['elasticsearch_ip'] = '127.0.0.1'
-normal.logstash['server']['elasticsearch_query'] = 'stack:elasticsearch*'
-normal.logstash['server']['graphite_ip'] = '127.0.0.1'
-normal.logstash['server']['graphite_query'] = 'stack:graphite*'
+# normal.logstash['server']['elasticsearch_ip'] = '127.0.0.1'
+# normal.logstash['server']['elasticsearch_query'] = 'stack:elasticsearch*'
+# normal.logstash['server']['graphite_ip'] = '127.0.0.1'
+# normal.logstash['server']['graphite_query'] = 'stack:graphite*'
 normal.logstash['server']['config_file'] = nil
 normal.logstash['server']['config_templates'] = ['o_server']
 
-normal.logstash['server']['config_templates_variables'] = { o_server: { es_server_ip: 'localhost',
+normal.logstash['server']['config_templates_variables'] = { o_server: { es_server_ip: 'elasticsearch-frontend-946485335.eu-west-1.elb.amazonaws.com',
                                                                         es_port: 443,
                                                                         es_index_prd: "aws-logstash-%{+YYYY.MM.dd}",
                                                                         es_flush_size: 500,
