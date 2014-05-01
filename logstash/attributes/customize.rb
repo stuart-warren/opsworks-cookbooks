38ccd6781fde9cab16940c21e898f8a69df23da7
Stuart Warren <stuart.warren@ocado.com>
2014-05-01 10:51:42 +0100

    test logstash customized_attributes

--- logstash/attributes/customize.rb
+++ logstash/attributes/customize.rb
@@ -3,15 +3,16 @@
 normal.logstash['install_zeromq'] = true
 normal.logstash['server']['enable_embedded_es'] = false
 
+# private variables can be kept out of your templates by putting in your chef json
 # normal.logstash['server']['elasticsearch_ip'] = 'elasticsearch-frontend-000000.eu-west-1.elb.amazonaws.com'
-# normal.logstash['server']['elasticsearch_port'] = 'elasticsearch-frontend-000000.eu-west-1.elb.amazonaws.com'
+# normal.logstash['server']['elasticsearch_port'] = '9200'
 normal.logstash['server']['config_file'] = nil
 normal.logstash['server']['config_templates'] = ['o_server']
 
 normal.logstash['server']['config_templates_variables'] = { o_server: { zmq_port: 2120,
                                                                         zmq_sockopts: '"ZMQ::RCVHWM",1000,"ZMQ::LINGER",10000, "ZMQ::RCVBUF", 87380',
                                                                         es_server_ip: node['logstash']['server']['elasticsearch_ip'],
-                                                                        es_port: node['logstash']['server']['elasticsearch_ip'],
+                                                                        es_port: node['logstash']['server']['elasticsearch_port'],
                                                                         es_index_prd: "aws-logstash-%{+YYYY.MM.dd}",
                                                                         es_flush_size: 500,
                                                                         es_idle_flush: 1,
