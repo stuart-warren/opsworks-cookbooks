81f43d746afb43b9d910110f4480d7d61fcb9a50
Stuart Warren <stuart.warren@ocado.com>
2014-05-01 14:54:31 +0100

    test tagging

--- aws-tag/recipes/ec2.rb
+++ aws-tag/recipes/ec2.rb
@@ -1,11 +1,8 @@
 
 include_recipe "aws"
-aws = data_bag_item("aws", "main")
 
 unless node['aws-tag']['tags'].empty? || node['aws-tag']['tags'].nil?
     aws_resource_tag node['ec2']['instance_id'] do
-        aws_access_key aws['aws_access_key_id']
-        aws_secret_access_key aws['aws_secret_access_key']
         tags(node['aws-tag']['tags'])
         action :update
     end
