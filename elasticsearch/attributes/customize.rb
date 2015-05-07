4df95fa7eba3011bab983143900f31e31bf41dc6
stuart.warren <stuart.warren@ocado.com>
2015-05-07 12:06:08 +0100

    test upgrade to 1.5.x

--- elasticsearch/attributes/customize.rb
+++ elasticsearch/attributes/customize.rb
@@ -1,11 +1,11 @@
 
 
-normal.elasticsearch['version'] = '1.1.0'
+normal.elasticsearch['version'] = '1.5.2'
 normal.elasticsearch['cluster']['name'] = 'elasticsearch-aws'
 # normal.elasticsearch['node']['master'] = true
 # normal.elasticsearch['node']['data'] = true
 normal.elasticsearch['logging']['action.discovery.ec2'] = 'INFO'
-normal.elasticsearch['plugins']['elasticsearch/elasticsearch-cloud-aws']['version'] = '2.1.0'
+normal.elasticsearch['plugins']['elastic/elasticsearch-cloud-aws']['version'] = '2.5.0'
 normal.elasticsearch['cloud']['aws']['region'] = 'eu-west-1'
 normal.elasticsearch['index']['number_of_shards'] = 3
 normal.elasticsearch['index']['number_of_replicas'] = 1
@@ -17,9 +17,9 @@ normal.elasticsearch['discovery']['zen']['ping']['multicast']['enabled'] = false
 normal.elasticsearch['limits']['nofile'] = 655350
 normal.elasticsearch['custom_config']['cluster.routing.allocation.awareness.attributes'] = 'aws_availability_zone'
 normal.elasticsearch['custom_config']['index.refresh_interval'] = '3s'
-normal.elasticsearch['custom_config']['indices.fielddata.cache.size'] = '15%'
-normal.elasticsearch['custom_config']['indices.fielddata.cache.expire'] = '1h'
-normal.elasticsearch['custom_config']['indices.cache.filter.size'] = '15%'
+# normal.elasticsearch['custom_config']['indices.fielddata.cache.size'] = '15%'
+# normal.elasticsearch['custom_config']['indices.fielddata.cache.expire'] = '1h'
+# normal.elasticsearch['custom_config']['indices.cache.filter.size'] = '15%'
 normal.elasticsearch['custom_config']['action.disable_shutdown'] = true
 normal.elasticsearch['custom_config']['script.disable_dynamic'] = true
 
@@ -36,9 +36,10 @@ if node[:opsworks][:instance][:layers].include? node['elasticsearch']['cluster']
     normal.elasticsearch['node']['data'] = false
     normal.elasticsearch['nginx']['allow_cluster_api'] = true
     normal.elasticsearch['nginx']['port'] = 80
-    normal.elasticsearch['plugins']['karmi/elasticsearch-paramedic'] = {}
-    normal.elasticsearch['plugins']['lmenezes/elasticsearch-kopf']['version'] = '0.5.5'
+    normal.elasticsearch['plugins']['lukas-vlcek/bigdesk'] = {}
+    normal.elasticsearch['plugins']['lmenezes/elasticsearch-kopf'] = {}
     normal.elasticsearch['plugins']['royrusso/elasticsearch-HQ'] = {}
+    normal.elasticsearch['plugins']['mobz/elasticsearch-head'] = {}
 
 end
 
@@ -48,7 +49,7 @@ if node[:opsworks][:instance][:layers].include? node['elasticsearch']['cluster']
     # datanode
     normal.elasticsearch['node']['master'] = false
     normal.elasticsearch['node']['data'] = true
-    normal.elasticsearch['custom_config']['indices.memory.index_buffer_size'] = '60%'
+    # normal.elasticsearch['custom_config']['indices.memory.index_buffer_size'] = '60%'
     normal.elasticsearch['custom_config']['http.enabled'] = false
 
 end
